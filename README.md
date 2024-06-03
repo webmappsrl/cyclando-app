a94532aae7d4afde7ea7730b3db5e08c9cfca6e4
bongiu <peppedeka@gmail.com>
2024-06-03 13:03:19 +0200

    chore: Update mockup link and add API endpoint for registration
    - Updated the mockup link in the README file.
    - Added an API endpoint for user registration in the environment.ts file.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cyclando-app
+mockup:https://xd.adobe.com/view/39adb970-99b4-4bb5-bdef-2d723400ef1d-6cfe/screen/f048dd9b-44a4-4daa-955c-0394378a9dc0/specs/
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api:'https://becyclando.dev.cyclando.com/api/v1/auth/register'
 };
 
 /*
