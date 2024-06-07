360579a6e339698ac0bab2320d745ffc7bab1101
Alessandro <alessandropeci@webmapp.it>
2024-06-07 16:04:12 +0200

    feat: launch of the user home page if previously logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private _router: Router) {}
+
+  ngOnInit(): void {
+    if ('access_token' in localStorage) this._router.navigate(['/user-home']);
+  }
 }
