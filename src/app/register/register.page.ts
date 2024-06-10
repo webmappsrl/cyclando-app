39130465d6cc975038be4334d79373b1c8a8029f
Alessandro <alessandropeci@webmapp.it>
2024-06-10 16:05:44 +0200

    refactor: change files position

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { register } from '../state/auth/auth.actions';
-import { stringMatchValidator } from '../validators/string-match.validators';
+import { stringMatchValidator } from '../shared/validators/string-match.validators';
 
 @Component({
   selector: 'app-register',
