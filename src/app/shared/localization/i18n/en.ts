047d7633a53787d1e140ba56c7762f5459b4a9fd
Alessandro <alessandropeci@webmapp.it>
2024-06-14 14:17:38 +0200

    refactor: add translation in login and register pages

--- src/app/shared/localization/i18n/en.ts
+++ src/app/shared/localization/i18n/en.ts
@@ -2,4 +2,12 @@ export const cyEN = {
   'Documenti di viaggio': 'Travel documents',
   'Non sono presenti documenti di viaggio': 'There are no travel documents',
   'VEDI DOCUMENTO DI VIAGGIO': 'SEE TRAVEL DOCUMENT',
+
+  'Email richiesta.': 'Email is required.',
+  'Formato email non valido.': 'Invalid email format.',
+  'Password richiesta.': 'Password is required.',
+  'La password deve avere almeno 8 caratteri.': '8 characters long.',
+  'Nome richiesto.': 'Name is required.',
+  'Cognome richiesto.': 'Surname is required.',
+  'Le password non corrispondono.': 'Passwords do not match.',
 };
--- src/app/shared/localization/i18n/it.ts
+++ src/app/shared/localization/i18n/it.ts
@@ -3,4 +3,8 @@ export const cyIT = {
   'Non sono presenti documenti di viaggio':
     'Non sono presenti documenti di viaggio',
   'VEDI DOCUMENTO DI VIAGGIO': 'VEDI DOCUMENTO DI VIAGGIO',
+  'The email must be a valid email address.':
+    "L'e-mail deve essere un indirizzo e-mail valido.",
+  'The selected email is invalid.': "L'e-mail selezionata non è valida.",
+  'Invalid credentials.': 'Credenziali non valide.',
 };
