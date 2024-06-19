29c3d1a4bb61cea9e5e26d7dacfa5168bf182e29
Alessandro <alessandropeci@webmapp.it>
2024-06-19 11:35:34 +0200

    feat: icon and splashscreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'it.webmapp.cyclando',
-  appName: 'cyclando',
-  webDir: 'www'
+  appName: 'Cyclando',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#fffbf7",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    }
+  }
 };
 
 export default config;
