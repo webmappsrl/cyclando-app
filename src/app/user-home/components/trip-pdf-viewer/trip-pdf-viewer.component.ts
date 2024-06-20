f3593931e6bab3bc859258a3ed8bb6d0ed391e2a
Alessandro <alessandropeci@webmapp.it>
2024-06-20 12:37:35 +0200

    refactor trip-pdf-viewer component

--- src/app/user-home/components/trip-pdf-viewer/trip-pdf-viewer.component.ts
+++ src/app/user-home/components/trip-pdf-viewer/trip-pdf-viewer.component.ts
@@ -1,7 +1,6 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ElementRef,
   Input,
   ViewChild,
   ViewEncapsulation,
@@ -10,6 +9,8 @@ import { ModalController } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 import { Trip } from '../../../models/user.model';
 import { NgxExtendedPdfViewerComponent } from 'ngx-extended-pdf-viewer';
+import { LangService } from '../../../shared/services/lang.service';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'cy-trip-pdf-viewer',
@@ -25,25 +26,32 @@ export class TripPdfViewerComponent {
   @Input() trip!: Trip;
 
   loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  maxZoom = 3;
-  minZoom = 0.8;
 
-  constructor(private _modalCtrl: ModalController) {}
+  constructor(
+    private _modalCtrl: ModalController,
+    private _langSvc: LangService,
+  ) {}
 
   ngAfterViewInit(): void {
-    setTimeout(() => {
-      this._pdfViewer.zoom = 'page-fit';
-    }, 0);
+    this._pdfViewer.zoom = 'page-fit';
   }
 
   cancel(): Promise<boolean> {
     return this._modalCtrl.dismiss(null, 'cancel');
   }
 
-  download(): void {
-    if (this.trip && this.trip.route.pdf_url) {
-      window.open(this.trip.route.pdf_url, '_blank');
-    }
+  async download(): Promise<void> {
+    console.log('share');
+    await Share.share({
+      title: this._langSvc.instant(
+        this.trip.route.name as { [lang: string]: string },
+      ),
+      dialogTitle: this._langSvc.instant(
+        this.trip.route.name as { [lang: string]: string },
+      ),
+      text: this._langSvc.instant('Questo è il mio viaggio con Cyclando!'),
+      url: this.trip.route.pdf_url,
+    });
   }
 
   pdfLoaded(): void {
